dff155246bb27d727cde698de09f0551488cd30c
Kuntal Ojha <kuntalojha2021@gmail.com>
2024-09-09 10:20:51 +0530

    Setup FrontEnd

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+function App() {
+  return (
+    <div>
+      <h1>Hello World from App.jsx from client </h1>
+    </div>
+  );
+}
+
+export default App;
