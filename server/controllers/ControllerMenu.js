64ed1bdd4c0d1f6cb053f6d5346b6bc282376c98
Kuntal Ojha <kuntalojha2021@gmail.com>
2024-09-12 13:05:53 +0530

    Update Code

--- server/controllers/ControllerMenu.js
+++ server/controllers/ControllerMenu.js
@@ -1,18 +1,3 @@
-// import Menu from '../models/ModelMenu.js';
-
-// export const getMenus = async (req, res) => {
-//   try {
-//     const menu = await Menu.find({});
-//     if (!menu) {
-//       return res.status(404).json({ message: 'Menu not found' });
-//     } else {
-//       res.status(200).json(menu);
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error fetching a single menu', error });
-//   }
-// };
-
 import Menu from '../models/ModelMenu.js';
 
 export const getMenus = async (req, res) => {
--- server/routes/RouteMenu.js
+++ server/routes/RouteMenu.js
@@ -1,10 +1,3 @@
-// import { Router } from 'express';
-// import { getMenus } from '../controllers/ControllerMenu.js';
-
-// const router = Router();
-// router.get('/getMenus', getMenus);
-// export default Router;
-
 import { Router } from 'express';
 import { getMenus } from '../controllers/ControllerMenu.js';
 
