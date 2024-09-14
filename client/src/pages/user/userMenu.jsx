19e3f5425cd2e1c4a3f7438f039ca26572d01d45
Kuntal Ojha <kuntalojha2021@gmail.com>
2024-09-14 13:16:01 +0530

    update menu

--- client/src/pages/user/userMenu.jsx
+++ client/src/pages/user/userMenu.jsx
@@ -1,13 +1,81 @@
 import React from 'react';
 import Footer from '@/components/common/Footer';
 import UserHeader from '@/components/user/userHeader';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
+import MenuImg from '../../assets/menu.png';
 const UserMenu = () => {
   return (
     <div>
       <UserHeader />
-      <div className='h-screen'>
-        <h1>User Menu</h1>
+      <div className='h-min lg:flex jitify-center'>
+        <div className='p-10 '>
+          <Card>
+            <CardHeader>
+              <img src={MenuImg} alt='menu image' />
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+        <div className='p-10 '>
+          <Card>
+            <CardHeader>
+              <img src={MenuImg} alt='menu image' />
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>{' '}
+        <div className='p-10 '>
+          <Card>
+            <CardHeader>
+              <img src={MenuImg} alt='menu image' />
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>{' '}
+        <div className='p-10 '>
+          <Card>
+            <CardHeader>
+              <img src={MenuImg} alt='menu image' />
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
       <Footer />
     </div>
