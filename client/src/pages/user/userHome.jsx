574b6d30818179a5f6db0ecf632e23ca96ba3845
Kuntal Ojha <kuntalojha2021@gmail.com>
2024-09-14 13:21:49 +0530

    little update on user home

--- client/src/pages/user/userHome.jsx
+++ client/src/pages/user/userHome.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 
-import Header from '@/components/common/Header';
 import Footer from '@/components/common/Footer';
 import HeroImg from '../../assets/hero.png';
 import Gallery from '@/components/common/Gallery';
+import UserHeader from '@/components/user/userHeader';
 const UserHome = () => {
   return (
     <div>
-      <Header />
+      <UserHeader />
       {/* Hero Section */}
       <div className='h-max overflow-hidden lg:ml-5 ml-0'>
         <div className='grid  max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 lg:grid-cols-2'>
@@ -46,44 +46,3 @@ const UserHome = () => {
 };
 
 export default UserHome;
-
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import { Button } from '@/components/ui/button';
-// import Header from '@/components/common/Header';
-// import Footer from '@/components/common/Footer';
-// import HeroImg from '../../assets/hero.png';
-
-// const UserHome = () => {
-//   return (
-//     <div>
-//       <Header />
-//       <div className='h-max overflow-hidden'>
-//         <div className='grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 lg:grid-cols-2'>
-//           <div>
-//             <h1 className='scroll-m-20 text-6xl font-extrabold tracking-tight lg:text-5xl'>
-//               Enjoy Our <br /> Delicious Meal
-//             </h1>
-//             <p className='leading-7 [&:not(:first-child)]:mt-6'>
-//               Once upon a time, in a far-off land, there was a very lazy king
-//               who spent all day lounging on his throne. One day, his advisors
-//               came to him with a problem: the kingdom was running out of money.
-//             </p>
-//             <Button
-//               asChild
-//               className='flex items-center mt-10 lg:mt-20 lg:h-12 w-4/5 md:w-2/4'
-//             >
-//               <NavLink to='/menu'>MENU</NavLink>
-//             </Button>
-//           </div>
-//           <div className='relative flex justify-center'>
-//             <img src={HeroImg} alt='Hero Image' className='w-4/5 h-auto z-10' />
-//           </div>
-//         </div>
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default UserHome;
